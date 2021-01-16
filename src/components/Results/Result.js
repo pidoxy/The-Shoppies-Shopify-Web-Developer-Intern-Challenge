f8f7bb3b9cba25a2dcd3b96da311aba7652c5c1c
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-01-16 19:29:15 +0000

    little fix

--- src/components/Results/Result.js
+++ src/components/Results/Result.js
@@ -8,7 +8,7 @@ const Result = (props) =>  {
     <ul>
       <li>{props.title} {props.year}</li>
       <button disabled={props.disableButtons && props.disableButtons.includes(props.imdbID)} id={props.buttonId} onClick={() => {
-        if(props.disableButtons.length > 4) return;
+        if(props.disableButtons.length > 4) return
         props.handleNominationsClick(props);
       props.setDisableButtons([...props.disableButtons, props.imdbID]);
 
